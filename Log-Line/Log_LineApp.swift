c51ce6d9d240e2d6df7979ceb9bbf7a4f04c000a
PetarSimonovic <petar@simonovic.com>
2021-04-03 13:21:59 +0100

    Initial Commit

---
+++ Log-Line/Log_LineApp.swift
@@ -0,0 +1,17 @@
+//
+//  Log_LineApp.swift
+//  Log-Line
+//
+//  Created by Petar Simonovic on 03/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct Log_LineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
