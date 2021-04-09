b8e3ccae88c7b86c34e6fb2b8edbf84c59635db0
PetarSimonovic <petar@simonovic.com>
2021-04-09 10:36:38 +0100

    About text expanded

--- Log-Line/Views/AboutView.swift
+++ Log-Line/Views/AboutView.swift
@@ -11,41 +11,40 @@ struct AboutView: View {
     
     var body: some View {
      
-        VStack(alignment: .leading) {
-            Text("A log line is a summary of a story. It needs to be concise - no more than one or two sentences - yet cover the narrative's central characters, story arc and conflicts.")
-            Spacer()
-                .frame(height: 10)
+        VStack {
+            VStack(alignment: .leading) {
+                Text("A log line is a summary of a story. Writers use log lines to help them focus on the story beats, and to pitch ideas to producers or publishers.\n\nA log line must be concise - no more than one or two sentences - yet cover the narrative's central characters, story arc and conflicts. Most importantly, it must hook the reader in.\n\nStory Mode generates short, compelling fictional scenarios designed to serve as a framework for narrative development.\n\nExcuse Mode creates tenable explanations that will justify offensive, abberrant or inconsiderate behaviour.")
+                Spacer()
+                    .frame(height: 10)
+                HStack (spacing: 0) {
+                    Text("")
+                    Text("Tap on ")
+                    Text("nouns")
+                        .foregroundColor(Color(UIColor.systemBackground))
+                        .background(Color(UIColor.label))
+                    Text(", ")
+                    Text("verbs")
+                        .foregroundColor(Color(UIColor.systemBackground))
+                        .background(Color(UIColor.label))
+                    Text(" and ")
+                }
+                HStack {
+                    Text("adjectives")
+                        .foregroundColor(Color(UIColor.systemBackground))
+                        .background(Color(UIColor.label))
+                    Text("to lock words")
+                }
+                HStack {
+                    Text("into your scenario and direct your narrative.")
+                }
+            }
+            .padding(50)
+            .font(.custom("Courier", size: 14.5))
             
-            Text("Most importantly, it must hook the reader in.")
-            Spacer()
-                .frame(height: 10)
-
-            Text("Story Mode generates short, compelling fictional scenarios designed to serve as a framework for narrative development.")
-            Spacer()
-                .frame(height: 10)
-            Text("Excuse Mode creates tenable explanations that can be used to justify offensive, abberrant or inconsiderate behaviour.")
-            Spacer()
-                .frame(height: 10)
-            HStack (spacing: 0) {
-                Text("Tap on ")
-                Text("nouns")
-                    .foregroundColor(Color(UIColor.systemBackground))
-                    .background(Color(UIColor.label))
-                Text(", ")
-                Text("verbs")
-                    .foregroundColor(Color(UIColor.systemBackground))
-                    .background(Color(UIColor.label))
-                Text(" and ")
-            }
-            HStack {
-                Text("adjectives")
-                    .foregroundColor(Color(UIColor.systemBackground))
-                    .background(Color(UIColor.label))
-                Text("to lock words.")
-            }
+            Text("Version 1.0")
+                .font(.custom("Courier", size: 10.5))
         }
-        .padding(50)
-        .font(.custom("Courier", size: 16.5))
+        
 
     }
     
